aabccaea04e47f4147c818af8f30d00e134799b4
Elvis <elvisnunez@me.com>
2025-09-06 11:30:21 +0200

    Fix order of words

--- Formula/llminty.rb
+++ Formula/llminty.rb
@@ -2,8 +2,8 @@ class Llminty < Formula
   desc "Produce a compact Swift repository bundle suitable for LLMs"
   homepage "https://github.com/3lvis/LLMinty"
   url "https://github.com/3lvis/LLMinty/releases/download/v0.1.0/llminty-v0.1.0-macos-arm64.tar.gz"
-  sha256 "21ca3bdd6d659790bb5b83df075b891215fbf34fb3ad34165d52845175c6eba6"
   version "0.1.0"
+  sha256 "21ca3bdd6d659790bb5b83df075b891215fbf34fb3ad34165d52845175c6eba6"
   license "MIT"
 
   def install
